ac477e6f21239877f132d1cc7169d168596a83a2
Abhishek Pathak <69243392+Abhishek-Pathak-Here@users.noreply.github.com>
2021-09-29 10:43:23 +0530

    Fixes

--- packages/client/src/pages/codeeditor/index.tsx
+++ packages/client/src/pages/codeeditor/index.tsx
@@ -1,33 +1,29 @@
-import React , {useEffect, useRef} from 'react'
+import React, { useEffect, useRef } from 'react'
 import * as monaco from 'monaco-editor'
 
-
-const codeEditor=()=>{
-    const divEl=useRef<HTMLDivElement>(null)
-    let editor:monaco.editor.IStandaloneCodeEditor
-    useEffect(() => {
-        if(divEl.current){
-            editor=monaco.editor.create(divEl.current,{
-                value:['This is the Typescript written here',"This is the second line of hte code here"].join('\n'),
-                language:'typescript',
-                dimension:{
-                    width:1200,
-                    height:"90vh",
-                },
-            })
-
-            editor.
-
-
+const codeEditor = () => {
+  const divEl = useRef<HTMLDivElement>(null)
+  let editor: monaco.editor.IStandaloneCodeEditor
+  useEffect(() => {
+    if (divEl.current) {
+      editor = monaco.editor.create(divEl.current, {
+        value: [
+          "import { compose, curry, isFunction } from '../utils';",
+          "import validators from '../validators';"
+        ].join('\n'),
+        language: 'typescript',
+        dimension: {
+          width: 1200,
+          height: 1200
         }
-        return () => {
-            editor.dispose()
-        }
-    }, [])    
+      })
+    }
+    return () => {
+      editor.dispose()
+    }
+  }, [])
 
-    return <div className="Editor" ref={divEl}> hasdfasdfasdfasdfasdf</div>
+  return <div className="Editor" ref={divEl} />
 }
 
 export default codeEditor
-
-
